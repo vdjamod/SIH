7aab5d38300c475ea37e86b51c82d31027823817
vipuljamod <vipuljamod122@gamil.com>
2024-09-03 13:45:21 +0530

    scommit

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router()
+const multer = require("multer");
+const pdfDetails = require('./pdfDetails')
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./files");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post("/upload-files", upload.single("file"), async (req, res) => {
+  console.log(req.file);
+  const title = req.body.title;
+  const fileName = req.file.filename;
+  try {
+    await PdfSchema.create({ title: title, pdf: fileName });
+    res.send({ status: "ok" });
+  } catch (error) {
+    res.json({ status: error });
+  }
+});
+
+router.get("/get-files", async (req, res) => {
+  try {
+    PdfSchema.find({}).then((data) => {
+      res.send({ status: "ok", data: data });
+    });
+  } catch (error) {}
+});
+
+module.exports = router
